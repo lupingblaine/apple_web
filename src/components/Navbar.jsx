d703a64e819fe0e4119dbf16ff92d88c841d379f
lupingblaine <lupingucr@gmail.com>
2025-01-07 20:06:49 +0800

    some improvements to the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
-import { appleImg } from '../utils';
+import { appleImg, bagImg, searchImg } from '../utils';
+import { navLists } from '../constants';
 
 const Navbar = () => {
   return (
-    <header>
-        <nav>
-            <img src={appleImg} />
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+        <nav className="flex w-full screen-max-width">
+            <img src={appleImg} alt="Apple" width={14} height={18}/>
+
+            <div className="flex  flex-1 justify-center max-sm:hidden">
+                {navLists.map((nav) => (
+                    <div key={nav}>
+                        {nav}
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <img src={searchImg} alt="search" width={18} height={18} />
+                <img src={bagImg} alt="bag" width={18} height={18} />
+            </div>
         </nav>
     </header>
   )
