1244334feb37a1091b7f611336333b2715bfc717
lupingblaine <lupingucr@gmail.com>
2025-01-09 01:20:07 +0800

    improvements in model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,7 +1,19 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
+import ModelView from "./ModelView";
+import { useState } from "react";
+import { yellowImg } from "../utils";
 
 const Model = () => {
+    const [Size, setSize] = useState('small');
+    const [model, setModel] = useState({
+        title:'iPhone 15 Pro in Natural Titanium',
+        color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+        img: yellowImg,
+    })
+
+// camera control for the model view
+
     useGSAP(() => {
         gsap.to('#heading', { y: 0, opacity: 1 })
     },[]);
@@ -14,7 +26,7 @@ const Model = () => {
 
           <div className="flex flex-col item-center my-5">
             <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-
+                <ModelView />
             </div>
           </div>
         </div>
