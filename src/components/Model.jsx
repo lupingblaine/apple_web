5dff7cd5978adde00d2e9f00bc35de1a75bae243
lupingblaine <lupingucr@gmail.com>
2025-01-09 12:45:22 +0800

    add click

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -90,7 +90,7 @@ const [largeRotation, setLargeRotation] = useState(0);
 
                     <button className="size-btn-container">
                         {sizes.map(({ label, value }) => (
-                            <span key={label}>
+                            <span key={label} className="size-btn" style={{ backgroundColor: size === value ? 'white' : 'transparent', color: size === value ? 'black' : 'white'}} onclick={() => setSize(value)}>
                                 {label}
                             </span>
                         ))}
