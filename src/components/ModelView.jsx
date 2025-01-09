e736966a9ca7045adea664891606f26f8e0ed929
lupingblaine <lupingucr@gmail.com>
2025-01-09 17:06:37 +0800

    iphone model and eslint

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,9 +1,10 @@
-import { PerspectiveCamera, View } from "@react-three/drei"
+import { Html, PerspectiveCamera, View } from "@react-three/drei"
 
 import Lights from "./Lights";
+import IPhone from './IPhone';
 import { Suspense } from "react";
 
-const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
     <View 
       index={index}
@@ -17,8 +18,8 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, siz
 
         <Lights />
 
-        <Suspense fallback={<div>Loading</div>}>
-          
+        <Suspense fallback={<Html><div>Loading</div></Html>}>
+        <IPhone />
         </Suspense>
       </View>
   )
