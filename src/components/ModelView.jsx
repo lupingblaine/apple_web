f96948dd2cb6415a78bbb8379889bc6842512f65
lupingblaine <lupingucr@gmail.com>
2025-01-09 15:43:00 +0800

    add lights

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,13 +1,24 @@
-import { View } from "@react-three/drei"
+import { PerspectiveCamera, View } from "@react-three/drei"
+
+import Lights from "./Lights";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
   return (
     <View 
       index={index}
       id={gsapType}
-      className="border-2 border-red-500"
+      className={`border-2 border-red-500 w-full h-full $ {index === 2} ? 'right-[-100%] : ''`}
       >
+        {/* Ambient Light */}
+        <ambientLight intensity={0.3} />
 
+        <PerspectiveCamera makedefault position={[0, 0, 4]} />
+
+        <Lights />
+
+        <Suspense fallback={<div>Loading</div>}>
+          
+        </Suspense>
       </View>
   )
 }
