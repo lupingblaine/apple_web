8b769f87a4e34fa25b383479e6770e98c2188abb
lupingblaine <lupingucr@gmail.com>
2025-01-10 19:33:41 +0800

    add plugin

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,20 @@
+import { useGSAP } from '@gsap/react'
 import React from 'react'
+import { animateWithGsap } from '../utils/animations';
 
 const Features = () => {
+    useGSAP(() => {
+        animateWithGsap('#feactures_title', { y: 0, opacity:1})
+    },[]);
+
   return (
-    <section className="h-full common-padding bg-zinc relative overflow-hidden">Features</section> 
+    <section className="h-full common-padding bg-zinc relative overflow-hidden">
+        <div className="screen-max-wdith">
+            <div className="mb-12 w-full">
+                <h1 id="feactures_title" className="section-heading" > Explore the full story.</h1>
+            </div>
+        </div>
+        </section> 
   )
 }
 
