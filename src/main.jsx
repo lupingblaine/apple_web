7d5f0f0e696fc26d8f7ce8be48df55d0af2ef7f3
lupingblaine <lupingucr@gmail.com>
2025-01-10 14:31:28 +0800

    Revert "install sentry"
    This reverts commit ff05f6201d083cee6f2efe2ad00cfb47c03ef1b9.

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://ccc4953e4e1fec35fc6eb045561f89ac@o4508613420384256.ingest.us.sentry.io/4508613423071232",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
