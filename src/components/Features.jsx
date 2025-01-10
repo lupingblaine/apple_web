da125cc5d69f2972ea5311b396e1ab95acdb8467
lupingblaine <lupingucr@gmail.com>
2025-01-10 20:23:45 +0800

    add text

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -13,6 +13,12 @@ const Features = () => {
             <div className="mb-12 w-full">
                 <h1 id="feactures_title" className="section-heading" > Explore the full story.</h1>
             </div>
+            <div className="flex flex-col justify-center items-center overflow-hidden">
+                <div className="mt-32 mb-24 pl-24">
+                    <h2 className="text-5xl lg:text-7xl font-semibold">iPhone.</h2>
+                    <h2 className="text-5xl lg:text-7xl font-semibold">Forged in titanium.</h2>
+                </div>
+            </div>
         </div>
         </section> 
   )
